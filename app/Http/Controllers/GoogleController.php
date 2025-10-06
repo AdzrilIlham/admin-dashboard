56751573e09d347ee04fe386af94aa6f42b09ff8
Adzril Ilham <adzril774@gmail.com>
2025-10-06 10:11:43 +0700

    Memperbaiki Login Google yang Tidak Ke Redirect

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+            
+            $user = User::where('email', $googleUser->getEmail())->first();
+            
+            if ($user) {
+                // Update google_id if not set
+                if (!$user->google_id) {
+                    $user->update(['google_id' => $googleUser->getId()]);
+                }
+            } else {
+                // Create new user
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'google_id' => $googleUser->getId(),
+                    'avatar' => $googleUser->getAvatar(),
+                    'password' => bcrypt(Str::random(16))
+                ]);
+            }
+            
+            Auth::login($user, true);
+            
+            return redirect()->route('dashboard');
+            
+        } catch (\Exception $e) {
+            return redirect()->route('login')->with('error', 'Unable to login with Google. Please try again.');
+        }
+    }
+}
